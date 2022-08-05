0565735ba127e561155a310f9fab11cd27e62077
Schratter, Markus <markus.schratter@v2c2.at>
2022-08-05 06:46:48 +0000

    Update install_ade.sh

--- install_ade.sh
+++ install_ade.sh
@@ -7,6 +7,9 @@ mkdir $MOLISENS_DIR/ade/MOLISENS/bagfiles
 git clone https://gitlab.v2c2.at/molisens/molisens_ws.git $MOLISENS_DIR/ade/MOLISENS/molisens_ws
 mkdir $MOLISENS_DIR/ade/MOLISENS/molisens_ws/src
 
+# write to .bashrc the extension to use the ADE environment
+echo "source $MOLISENS_DIR/ade/extensions.sh" >> ~/.bashrc
+
 # copy local git configuration into the ADE environment 
 cp ~/.gitconfig $MOLISENS_DIR/ade/.gitconfig
 
