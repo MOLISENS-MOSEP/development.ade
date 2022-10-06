a4202d676b289a46d7e41d0a700ba44ab27b6e36
thomasgoelles <thomas.goelles@gmail.com>
2022-10-06 15:41:31 +0200

    added support for MacOS

--- extensions.sh
+++ extensions.sh
@@ -1,22 +1,26 @@
 #!/bin/sh
 
 # ADE
-export MOLISENS_DIR=$HOME/projects/MOLISENS
 export ADE_MOLISENS_PATH=$MOLISENS_DIR/ade
 
 PATH=$PATH:$ADE_MOLISENS_PATH
 
 
-
-
-ARCHITECTURE=$(uname -i)
+ARCHITECTURE=$(uname -m)
 if [[ $ARCHITECTURE == x86_64* ]]; then
   ADE_VERSION=ade+x86_64
-  #export ADE_DISABLE_NVIDIA_DOCKER=true  # If you want to use CUDA inside ADE, comment this line!  
-elif [[ $ARCHITECTURE == aarch64* ]]; then
+  #export ADE_DISABLE_NVIDIA_DOCKER=true  # If you want to use CUDA inside ADE, comment this line!
+elif [[ $ARCHITECTURE == aarch64* ]] then
   ADE_VERSION=ade+aarch64
   export ADE_DISABLE_NVIDIA_DOCKER=true  # If you want to use CUDA inside ADE, comment this line!
+elif  [[ $ARCHITECTURE == arm64 ]]; then
+  # asuming OSX with arm processor
+  # install ade with https://ade-cli.readthedocs.io/en/latest/install.html
+  ADE_VERSION=ade
+  export ADE_DISABLE_NVIDIA_DOCKER=true
 fi
+
+echo $ADE_VERSION
 
 # ade environment
 alias molisense_ade_update="export ADE_NAME=molisens && cd $ADE_MOLISENS_PATH && $ADE_VERSION --rc $ADE_MOLISENS_PATH/.aderc_$ARCHITECTURE start --update"
--- install_ade.sh
+++ install_ade.sh
@@ -17,9 +17,6 @@ write_shrc()
 }
 
 
-
-export MOLISENS_DIR=$HOME/projects/MOLISENS
-
 mkdir $MOLISENS_DIR/ade/MOLISENS
 mkdir $MOLISENS_DIR/ade/MOLISENS/bagfiles
 git clone https://gitlab.v2c2.at/molisens/molisens_ws.git $MOLISENS_DIR/ade/MOLISENS/molisens_ws
@@ -28,6 +25,6 @@ mkdir $MOLISENS_DIR/ade/MOLISENS/molisens_ws/src
 # write to .bashrc the extension to use the ADE environment
 write_shrc "source $MOLISENS_DIR/ade/extensions.sh"
 
-# copy local git configuration into the ADE environment 
+# copy local git configuration into the ADE environment
 cp ~/.gitconfig $MOLISENS_DIR/ade/.gitconfig
 
