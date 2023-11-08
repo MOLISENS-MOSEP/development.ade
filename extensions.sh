1aba78c7ce6300d9666a40a7dfec3bcc55abf29c
Nikolic, Oliver <oliver.nikolic@v2c2.at>
2023-11-08 12:39:42 +0000

    Minor QOL improvements

--- extensions.sh
+++ extensions.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # ADE
-export ADE_MOLISENS_PATH=$MOLISENS_DIR/ade
+export ADE_MOLISENS_PATH=$MOLISENS_DIR/ade/MOLISENS/molisens_ws
 
 PATH=$PATH:$ADE_MOLISENS_PATH
 
@@ -23,8 +23,8 @@ fi
 echo $ADE_VERSION
 
 # ade environment
-alias molisense_ade_update="export ADE_NAME=molisens && cd $ADE_MOLISENS_PATH && $ADE_VERSION --rc $ADE_MOLISENS_PATH/.aderc_$ARCHITECTURE start --update"
-alias molisense_ade_start="export ADE_NAME=molisens && $ADE_VERSION --rc $ADE_MOLISENS_PATH/.aderc_$ARCHITECTURE start --enter"
-alias molisense_ade_enter="export ADE_NAME=molisens && molisense_ade_start; $ADE_VERSION enter"
-alias molisense_ade_stop="export ADE_NAME=molisens && $ADE_VERSION stop"
+alias molisens_ade_update="export ADE_NAME=molisens && cd $ADE_MOLISENS_PATH && $ADE_VERSION --rc $ADE_MOLISENS_PATH/.aderc_$ARCHITECTURE start --update"
+alias molisens_ade_start="export ADE_NAME=molisens && $ADE_VERSION --rc $ADE_MOLISENS_PATH/.aderc_$ARCHITECTURE start --enter"
+alias molisens_ade_enter="export ADE_NAME=molisens && molisens_ade_start; $ADE_VERSION enter"
+alias molisens_ade_stop="export ADE_NAME=molisens && $ADE_VERSION stop"
 
